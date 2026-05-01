acb5344c9a2601420db3acf7da950d52bd8ca5b4
Peter Jakobs <pjakobs@mobileme.de>
2026-05-01 07:47:10 +0200

    Initial interface contract: display_driver_interface.h and documentation

---
+++ src/esp_lcd_adapter.c
@@ -0,0 +1,10 @@
+/**
+ * esp_lcd_adapter.c
+ *
+ * Bridge between LVGL runtime and esp_lcd drivers.
+ * This file will contain frame buffer management, LVGL callbacks, and scheduling logic.
+ *
+ * TODO: Implement LVGL display driver integration.
+ */
+
+#include "esp_lcd_adapter/display_driver_interface.h"
